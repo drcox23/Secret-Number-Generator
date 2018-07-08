a37cf37d9df4dd0fcb750e074eb9fa279eca59c7
doug <drcox23@gmail.com>
2018-07-07 15:24:25 -1000

    initial commit

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,13 @@
 'use strict';
 module.exports = function() {
-  
-};
+    const randNum = Math.floor(Math.random()*1000000);
+    return function (){
+        return randNum;
+    }
+    };
+//   const secretNumber = function(){
+//     return function (){
+//         let secretNumberGenerator = Math.floor(Math.random()*1000000);
+    // }
+    // return secretNumber;
+    // secretNumber();
